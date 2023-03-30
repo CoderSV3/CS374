66d7a768a31e90fad757c11755ab416b5ec7dda6
CoderSV3 <samuelviveros3@gmail.com>
2023-03-30 14:03:48 -0400

    Updated AndroidManifest.xml with Converter.java, added onClick() method to MainActivity.java, and fixed some XML code while updating values in their respective XML files and directories

--- ConversionSamuelViverosV3/app/src/main/java/com/example/conversionsamuelviverosv3/Converter.java
+++ ConversionSamuelViverosV3/app/src/main/java/com/example/conversionsamuelviverosv3/Converter.java
@@ -1,4 +1,33 @@
 package com.example.conversionsamuelviverosv3;
 
-public class Converter {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class Converter extends AppCompatActivity {
+
+    private TextView prompt;
+    private TextView dollars;
+    private TextView amountDisplayed;
+    private TextView retry;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.converter);
+        prompt = findViewById(R.id.prompt);
+        dollars = findViewById(R.id.dollarsLabel);
+        amountDisplayed = findViewById(R.id.amountDisplayed);
+        retry = findViewById(R.id.retry);
+
+    }
+
+
+
+    public void convert(View view) {
+
+    }
 }
--- ConversionSamuelViverosV3/app/src/main/java/com/example/conversionsamuelviverosv3/MainActivity.java
+++ ConversionSamuelViverosV3/app/src/main/java/com/example/conversionsamuelviverosv3/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.conversionsamuelviverosv3;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    private Button begin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        begin = findViewById(R.id.begin);
+    }
+
+    protected void onClick(@NonNull View view) {
+        startConverter(begin);
     }
 
     public void startConverter(View view) {
