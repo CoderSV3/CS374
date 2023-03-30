fc80e369285c92eed08aba2ed5aa3095030eb049
CoderSV3 <samuelviveros3@gmail.com>
2023-03-30 14:27:20 -0400

    Added two screenshots, updated Converter.java, and added my midterm photo as per exam instructions

--- ConversionSamuelViverosV3/app/src/main/java/com/example/conversionsamuelviverosv3/Converter.java
+++ ConversionSamuelViverosV3/app/src/main/java/com/example/conversionsamuelviverosv3/Converter.java
@@ -1,18 +1,27 @@
 package com.example.conversionsamuelviverosv3;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.Scanner;
+
 public class Converter extends AppCompatActivity {
 
+    public static final String INPUT = "com.example.converstionsamuelviverosv3.extra.INPUT";
     private TextView prompt;
     private TextView dollars;
     private TextView amountDisplayed;
     private TextView retry;
+
+    private Button convert;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +31,20 @@ public class Converter extends AppCompatActivity {
         dollars = findViewById(R.id.dollarsLabel);
         amountDisplayed = findViewById(R.id.amountDisplayed);
         retry = findViewById(R.id.retry);
+        convert = findViewById(R.id.convert_button);
+        editText = findViewById(R.id.input_field);
+
+        String input = editText.getText().toString();
 
     }
 
+    protected void onClick(@NonNull View view) {
 
+        convert(convert);
+
+    }
 
     public void convert(View view) {
-
+        
     }
 }
